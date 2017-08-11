2a0727e906710470c2f0f957e8b5d195e96534a8
Anthony Diperna <anthonydiperna@northwesternmutual.com>
2017-08-11 13:14:40 -0500

    hmm

--- .travis/deploy-dockerhub.sh
+++ .travis/deploy-dockerhub.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-echo $TRAVIS_BRANCH
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
@@ -8,5 +7,6 @@ else
     TAG="$TRAVIS_BRANCH"
 fi
 
+REPO_NAME="dipernaa/pokedex-react"
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 docker push $TRAVIS_REPO_SLUG
